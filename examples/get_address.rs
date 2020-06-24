94823459ee08ff9a4a0326137a5e6f2b4d7fdc80
Auyer <rafael@rcpassos.me>
2020-06-24 03:03:38 -0300

    removed -rs from lib name in Cargo (unecessary)

--- examples/get_address.rs
+++ examples/get_address.rs
@@ -1,9 +1,9 @@
 // examples/get_address.rs
-extern crate lagoinha_rs;
+extern crate lagoinha;
 
 extern crate tokio;
 #[tokio::main]
 async fn main() {    
-    let addr = lagoinha_rs::get_address("04569901").await;
+    let addr = lagoinha::get_address("04569901").await;
     println!("{:#?}", addr);
 }
--- examples/standalone_services.rs
+++ examples/standalone_services.rs
@@ -1,18 +1,18 @@
 // examples/standalone_services.rs
-extern crate lagoinha_rs;
+extern crate lagoinha;
 
 extern crate tokio;
 #[tokio::main]
 async fn main() {    
-    let addr = lagoinha_rs::services::viacep::request("04569901").await;
+    let addr = lagoinha::services::viacep::request("04569901").await;
     println!("\nviacep");
     println!("{:#?}", addr);
 
-    let addr = lagoinha_rs::services::correios::request("04569901").await;
+    let addr = lagoinha::services::correios::request("04569901").await;
     println!("\ncorreios");
     println!("{:#?}", addr);
 
-    let addr = lagoinha_rs::services::cepla::request("04569901").await;
+    let addr = lagoinha::services::cepla::request("04569901").await;
     println!("\ncepla");
     println!("{:#?}", addr);
 }
